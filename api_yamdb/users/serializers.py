b6b278bfb34a9d5f2cd22b53a4bbfee0fe4f6ed1
Удалов В.С <udalov@gbu.volgo-balt.ru>
2022-09-23 14:48:09 +0300

    Yandb-final. Ver.1.3.1.

--- api_yamdb/users/serializers.py
+++ api_yamdb/users/serializers.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework.validators import UniqueValidator
+from rest_framework_simplejwt.tokens import AccessToken
 
 from .models import User
 
